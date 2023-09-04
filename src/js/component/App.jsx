51e191b4165f9451bc735a60b2c44a8876645bad
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-09-04 22:41:54 +0000

    Diseño listo pero aún no es responsive. Sólo en pantallas xxl

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -8,70 +8,79 @@ export default function App() {
   return (
     <div className="bigContainer">
       <div className="littleContainer">
-        <div className="inputs">
-          {/* Comienzo de dropdown */}
-          <div class="btn-group dropend h-25 mt-3 ms-4">
-            <button
-              id="dropdownButton"
-              type="button"
-              class="btn dropdown-toggle"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Elige tu sección
-            </button>
-            <ul class="dropdown-menu">
-              <li>
-                <a class="dropdown-item" href="#">
-                  Grateful for
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#">
-                  Today's priorities
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#">
-                  To Do List
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#">
-                  Notes
-                </a>
-              </li>
-            </ul>
+        <div className="inputsContainer">
+          <div className="inputs">
+            {/* Comienzo de dropdown */}
+            <div class="btn-group dropend h-25 mt-3 ms-4">
+              <button
+                id="dropdownButton"
+                type="button"
+                class="btn dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Elige tu sección
+              </button>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Grateful for
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Today's priorities
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    To Do List
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Notes
+                  </a>
+                </li>
+              </ul>
+            </div>
+            {/*Fin de dropdown */}
+            <div>
+              <textarea
+                rows="3"
+                cols="70"
+                id="textareaContent"
+                className="mt-3"
+              ></textarea>
+            </div>
           </div>
-          {/*Fin de dropdown */}
-          <textarea
-            rows="3"
-            cols="70"
-            id="textareaContent"
-            className="mt-3"
-          ></textarea>
-          <input
-            id="submitButton"
-            className="btn ms-3"
-            type="submit"
-            value="Submit"
-          ></input>
+          <div class="submitContainer">
+            <input
+              className="submitButton btn ms-3"
+              type="submit"
+              value="Submit"
+            ></input>
+          </div>
         </div>
-        <div className="grateful">
-          <div className="gratefulTitle">TODAY, I AM GRATEFUL FOR</div>
-          <div className="gratefulContent"></div>
-        </div>
-        <div className="priorities">
-          <div className="prioritiesTitle">PRIORITIES</div>
-          <div className="prioritiesContent"></div>
-        </div>
-        <div className="toDo">
-          <div className="toDoTitle">TO DO LIST</div>
-          <div className="toDoContent"></div>
-        </div>
-        <div className="notes">
-          <div className="notesTitle">NOTES</div>
-          <div className="notesContent"></div>
+        <div className="listsContainer">
+          <div className="gratefulAndPriorities">
+            <div className="grateful">
+              <div className="gratefulTitle">TODAY, I AM GRATEFUL FOR</div>
+              <div className="gratefulContent"></div>
+            </div>
+            <div className="priorities">
+              <div className="prioritiesTitle">PRIORITIES</div>
+              <div className="prioritiesContent"></div>
+            </div>
+          </div>
+          <div className="toDo">
+            <div className="toDoTitle">TO DO LIST</div>
+            <div className="toDoContent"></div>
+          </div>
+          <div className="notes">
+            <div className="notesTitle">NOTES</div>
+            <div className="notesContent"></div>
+          </div>
         </div>
       </div>
     </div>
