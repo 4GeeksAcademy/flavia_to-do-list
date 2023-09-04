3a8bdb50565445b73ebc15084a1072801a4974c4
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-09-04 19:57:01 +0000

    Esqueleto CSS listo. Faltan detalles

--- src/js/component/App.jsx
+++ src/js/component/App.jsx
@@ -1,37 +1,79 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import NewTask from "./NewTask";
 import TaskList from "./TaskList";
+import "./App.css";
 
 //create your first component
 export default function App() {
-	const [newTask, setNewTask] = useState({});
-	const handleChange = ({ target }) => {
-	  const { name, value } = target;
-	  setNewTask((prev) => ({ ...prev, id: Date.now(), [name]: value }));
-	};
-  
-	const [allTasks, setAllTasks] = useState([]);
-	const handleSubmit = (event) => {
-	  event.preventDefault();
-	  if (!newTask.title) return;
-	  setAllTasks((prev) => [newTask, ...prev]);
-	  setNewTask({});
-	};
-	const handleDelete = (taskIdToRemove) => {
-	  setAllTasks((prev) => prev.filter(
-		(task) => task.id !== taskIdToRemove
-	  ));
-	};
-  
-	return (
-	  <main>
-		<h1>Tasks</h1>
-		<NewTask
-		  newTask={newTask}
-		  handleChange={handleChange}
-		  handleSubmit={handleSubmit}
-		/>
-		<TaskList allTasks={allTasks} handleDelete={handleDelete} />
-	  </main>
-	);
-  }
+  return (
+    <div className="bigContainer">
+      <div className="littleContainer">
+        <div className="inputs">
+          {/* Comienzo de dropdown */}
+          <div class="btn-group dropend h-25 mt-3 ms-4">
+            <button
+              id="dropdownButton"
+              type="button"
+              class="btn dropdown-toggle"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Elige tu sección
+            </button>
+            <ul class="dropdown-menu">
+              <li>
+                <a class="dropdown-item" href="#">
+                  Grateful for
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  Today's priorities
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  To Do List
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  Notes
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/*Fin de dropdown */}
+          <textarea
+            rows="3"
+            cols="70"
+            id="textareaContent"
+            className="mt-3"
+          ></textarea>
+          <input
+            id="submitButton"
+            className="btn ms-3"
+            type="submit"
+            value="Submit"
+          ></input>
+        </div>
+        <div className="grateful">
+          <div className="gratefulTitle">TODAY, I AM GRATEFUL FOR</div>
+          <div className="gratefulContent"></div>
+        </div>
+        <div className="priorities">
+          <div className="prioritiesTitle">PRIORITIES</div>
+          <div className="prioritiesContent"></div>
+        </div>
+        <div className="toDo">
+          <div className="toDoTitle">TO DO LIST</div>
+          <div className="toDoContent"></div>
+        </div>
+        <div className="notes">
+          <div className="notesTitle">NOTES</div>
+          <div className="notesContent"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/js/component/NewTask/NewTask.jsx
+++ src/js/component/NewTask/NewTask.jsx
@@ -1,26 +1,2 @@
 import React from "react";
 import "./newTask.css"
-
-export default function NewTask({ newTask, handleChange, handleSubmit }) {
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        name="title"
-        placeholder="New task"
-        value={newTask.title || ""}
-        onChange={handleChange}
-      />
-      {!newTask.title ? null : (
-        <>
-          <textarea
-            name="description"
-            placeholder="Details..."
-            value={newTask.description || ""}
-            onChange={handleChange}
-          />
-          <button type="submit">Add Task</button>
-        </>
-      )}
-    </form>
-  );
-}
